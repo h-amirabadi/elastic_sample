d4dbaeaa6e76bce435422f6285fe2ea085082bf4
Hossein <h.amirabadi.n@gmail.com>
2024-10-06 16:23:01 +0330

    Optimize imports

--- src/main/java/com/h/elastic_sample/controller/MultilangDocumentController.java
+++ src/main/java/com/h/elastic_sample/controller/MultilangDocumentController.java
@@ -6,7 +6,6 @@ import com.h.elastic_sample.service.MultilangDocumentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
--- src/main/java/com/h/elastic_sample/data/dto/MultilangDocumentDto.java
+++ src/main/java/com/h/elastic_sample/data/dto/MultilangDocumentDto.java
@@ -1,8 +1,8 @@
 package com.h.elastic_sample.data.dto;
 
-import lombok.*;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
